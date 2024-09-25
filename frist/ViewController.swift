2c3174849b31d00d4ad12215ff624a2d72e144b7
xianchen <xianchen@163.com>
2024-09-25 20:47:58 +0800

    Initial Commit

---
+++ frist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  frist
+//
+//  Created by Lucky on 2024/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
